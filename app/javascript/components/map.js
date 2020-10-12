a026951e58c274f40cfe78e64e843366ef94e9b5
Keiichi Katsuno <keichi031605@gmail.com>
2020-10-12 18:20:59 +1100

    MODI: Layout of landing page, finidshed

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -2,12 +2,29 @@ import mapboxgl from 'mapbox-gl';
 
 const showMap = () => {
   const map_div = document.getElementById('map');
-  if (map_div) {
+    if (map_div) {
     mapboxgl.accessToken = process.env.MAPBOX_API_KEY
-      window.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-      });
+    var center_point = [138.594709,
+    -34.976377]
+    window.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: center_point,
+      zoom: 12
+    });
+      var marker = new mapboxgl.Marker()
+      var abd_point = [138.594709,
+    -34.976377]
+
+      var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+        "<strong>Adelaid Bat Doctor</strong><p>We Are Here!</p>"
+        );
+
+      new mapboxgl.Marker(marker)
+      .setLngLat(abd_point)
+      .setPopup(popup)
+      .addTo(map);
+
     }
   };
 
