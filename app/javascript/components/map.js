9b370ea9d1fd78723f4e42b56c94f915e02e1c22
Keiichi Katsuno <keichi031605@gmail.com>
2020-10-15 03:47:58 +1100

    MODI: Updated map.js popup on the map

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -4,8 +4,7 @@ const showMap = () => {
   const map_div = document.getElementById('map');
     if (map_div) {
     mapboxgl.accessToken = process.env.MAPBOX_API_KEY
-    var center_point = [138.594709,
-    -34.976377]
+    var center_point = [138.594709, -34.976377]
     window.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -13,18 +12,15 @@ const showMap = () => {
       zoom: 12
     });
       var marker = new mapboxgl.Marker()
-      var abd_point = [138.594709,
-    -34.976377]
+      var abd_point = [138.594709, -34.976377]
 
       var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-        "<strong>Adelaid Bat Doctor</strong><p>We Are Here!</p>"
+        '<strong>Adelaid Bat Doctor</strong><p>We Are <a href="https://www.google.com/maps/dir/-37.791367,144.9281504/-34.97638,138.59471/@-36.2795437,137.2639272,6z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0" target="_blank">Here!</a></p>'
         );
-
       new mapboxgl.Marker(marker)
       .setLngLat(abd_point)
       .setPopup(popup)
       .addTo(map);
-
     }
   };
 
